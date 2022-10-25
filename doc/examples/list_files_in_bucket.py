9c32ee3abff2666df212e461df9b71ef8ec09ea4
Nicola Coretti <nicola.coretti@exasol.com>
2022-10-25 09:20:17 +0200

    Update example

--- doc/examples/list_files_in_bucket.py
+++ doc/examples/list_files_in_bucket.py
@@ -1,21 +1,19 @@
 from exasol.bucketfs import Service, Bucket
 
 URL = "http://localhost:6666"
-AUTHENTICATOR = {'default': {'username': 'w', 'password': 'write'}}
+CREDENTAILS = {'default': {'username': 'w', 'password': 'write'}}
 BUCKET_NAME = 'default'
 
 # 1. List files through bucket of service object
-# TODO: Update service constructor to support authenticator
-bucketfs = Service(URL, AUTHENTICATOR)
-# TODO: Implement indirect construction
+bucketfs = Service(URL, CREDENTAILS)
 files = [file for file in bucketfs['default']]
 
 # 2. List files using the bucket class
 bucket = Bucket(
     name=BUCKET_NAME,
     service=URL,
-    username=AUTHENTICATOR[BUCKET_NAME]['username'],
-    password=AUTHENTICATOR[BUCKET_NAME]['password']
+    username=CREDENTAILS[BUCKET_NAME]['username'],
+    password=CREDENTAILS[BUCKET_NAME]['password']
 )
 
 files = [file for file in bucket]
