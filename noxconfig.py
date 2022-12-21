6feb262806bc6297478291b06cd1c8d796c00940
Nicola Coretti <nicola.coretti@exasol.com>
2022-12-21 12:14:17 +0100

    Add pre_integration_tests_hook

--- noxconfig.py
+++ noxconfig.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import (
     Any,
     MutableMapping,
@@ -21,6 +22,29 @@ class Config:
         _session: Session, _config: Config, _context: MutableMapping[str, Any]
     ) -> bool:
         """Implement if project specific behaviour is required"""
+        with TemporaryDirectory() as tmp_dir:
+            tmp_dir = Path(tmp_dir)
+            checkout_name = "ITDE"
+            with _session.chdir(tmp_dir):
+                _session.run(
+                    "git",
+                    "clone",
+                    "https://github.com/exasol/integration-test-docker-environment.git",
+                    checkout_name,
+                )
+            with _session.chdir(tmp_dir / checkout_name):
+                _session.run(
+                    "./start-test-env",
+                    "spawn-test-environment",
+                    "--environment-name",
+                    "test",
+                    "--database-port-forward",
+                    "8888",
+                    "--bucketfs-port-forward",
+                    "6666",
+                    "--db-mem-size",
+                    "4GB",
+                )
         return True
 
     @staticmethod
