b26fda1e89c873660e0e3496a6d76d6f8cb182dc
mibe <mikhail.beck@exasol.com>
2024-05-02 10:37:00 +0100

    #118 Fixed the factory integration test.

--- tests/integration_tests/without_db/test_bucketfs_factory.py
+++ tests/integration_tests/without_db/test_bucketfs_factory.py
@@ -16,8 +16,8 @@ def test_factory_http_not_verify(default_bucket_config):
     file_name = 'test_factory_http_default/geography.fact'
     content = 'Munich is the capital of Bavaria'
     bfs_location.upload_string_to_bucketfs(file_name, content)
-    assert file_name in bfs_location.list_files_in_bucketfs()
+    assert file_name in bfs_location.list_files_in_bucketfs('')
     content_back = bfs_location.download_from_bucketfs_to_string(file_name)
     assert content_back == content
     bfs_location.delete_file_in_bucketfs(file_name)
-    assert file_name not in bfs_location.list_files_in_bucketfs()
+    assert file_name not in bfs_location.list_files_in_bucketfs('')
