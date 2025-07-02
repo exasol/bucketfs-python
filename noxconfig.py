448eb730e63670f655eb80ee7433446e610ce9b6
Ariel Schulz <ariel.schulz@exasol.com>
2025-07-02 14:13:06 +0200

    Update dependencies & fix typos

--- noxconfig.py
+++ noxconfig.py
@@ -9,12 +9,14 @@ from typing import Iterable
 class Config:
     root: Path = Path(__file__).parent
     doc: Path = Path(__file__).parent / "doc"
+    source: Path = Path("exasol/bucketfs")
     version_file: Path = Path(__file__).parent / "exasol" / "bucketfs" / "version.py"
     path_filters: Iterable[str] = (
         "dist",
         ".eggs",
         "venv",
     )
+    python_versions = ["3.10", "3.11", "3.12", "3.13"]
 
 
 PROJECT_CONFIG = Config()
